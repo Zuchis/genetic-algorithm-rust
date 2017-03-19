1aa98276a7f7250a653020d4789af4f3908c9d67
Zuchis <liquimaster@gmail.com>
2017-03-19 16:54:31 +0000

    added evaluation functions

--- src/population.rs
+++ src/population.rs
@@ -5,11 +5,12 @@ use population::rand::distributions::{IndependentSample, Range};
 
 #[derive(Debug, Clone)]
 pub struct Population<T> {
-    pop_size: u64,
-    ind_size: u64,
-    individuals: Vec<T>,
-    lower_bound: f64,
-    upper_bound: f64,
+    pub pop_size: u64,
+    pub ind_size: u64,
+    pub individuals: Vec<T>,
+    pub fit_array: Vec<f64>,
+    pub lower_bound: f64,
+    pub upper_bound: f64,
 }
 
 #[allow(dead_code)]
@@ -22,6 +23,7 @@ impl<T> Population<T> {
             lower_bound: lb,
             upper_bound: ub,
             individuals: Vec::new(),
+            fit_array: vec![0.0; pop_size_ as usize],
         }
     }
 
@@ -31,6 +33,16 @@ impl<T> Population<T> {
 
     pub fn set(&mut self, i: u64, j: u64, value: T) {
         self.individuals[((i * self.ind_size) + j) as usize] = value;
+    }
+
+    pub fn evaluate_individual(&mut self, ind: u64, f: &Fn(&Population<T>,u64) -> f64) {
+        self.fit_array[ind as usize] = f(&self, ind);
+    }
+
+    pub fn evaluate_all(&mut self, f: &Fn(&Population<T>,u64) -> f64) {
+        for i in 0 .. self.pop_size {
+            self.fit_array[i as usize] = f(&self,i);
+        }
     }
 }
 
@@ -58,7 +70,13 @@ impl Population<i64> {
             }
             println!("");
         }
+
+        println!["Population's fitness:"];
+        for i in 0 .. self.pop_size {
+            print!(" {} ",self.fit_array[i as usize]);
+        }
     }
+
 }
 
 #[allow(dead_code)]
@@ -67,6 +85,7 @@ impl Population<f64> {
         self.individuals = vec![0.0; (self.pop_size * self.ind_size) as usize];
         let between = Range::new(self.lower_bound, self.upper_bound);
         let mut rng = rand::thread_rng();
+
         for i in 0..self.pop_size {
             for j in 0..self.ind_size {
                 let value: f64 = between.ind_sample(&mut rng);
@@ -82,13 +101,18 @@ impl Population<f64> {
             }
             println!("");
         }
+        println!["Population's fitness:"];
+        for i in 0 .. self.pop_size {
+            print!(" {} ",self.fit_array[i as usize]);
+        }
     }
+
 }
 
 
 #[allow(dead_code)]
 impl Population<bool> {
-    fn initialize(&mut self) {
+    pub fn initialize(&mut self) {
         self.individuals = vec![false; (self.pop_size * self.ind_size) as usize];
         let mut rng = rand::thread_rng();
         for i in 0..self.pop_size {
@@ -105,6 +129,11 @@ impl Population<bool> {
                 print!("{} ", self.access(i, j));
             }
             println!("");
+        }
+
+        println!["Population's fitness:"];
+        for i in 0 .. self.pop_size {
+            print!(" {} ",self.fit_array[i as usize]);
         }
     }
 }
