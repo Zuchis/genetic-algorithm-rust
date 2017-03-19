6c1c39f2ae6b84c573788414f48842c90e8c11a9
Zuchis <liquimaster@gmail.com>
2017-03-19 16:53:50 +0000

    now created a main loop for each gene type

---
+++ src/ga.rs
@@ -0,0 +1,23 @@
+use population::Population;
+
+use ::fitness;
+
+pub fn int_loop(p_size: u64, i_size: u64, lb: f64, ub: f64) {
+    let mut pop = Population::<i64>::new(p_size,i_size,lb,ub);
+    pop.initialize();
+    pop.evaluate_all(&fitness::int_parity_alternate);
+    pop.print();
+}
+
+pub fn float_loop(p_size: u64, i_size: u64, lb: f64, ub: f64) {
+    let mut pop = Population::<f64>::new(p_size,i_size,lb,ub);
+    pop.initialize();
+    pop.print();
+}
+
+pub fn bin_loop(p_size: u64, i_size: u64) {
+    let mut pop = Population::<bool>::new(p_size,i_size,0.0,0.0);
+    pop.initialize();
+    pop.evaluate_all(&fitness::binary_alternate);
+    pop.print();
+}
