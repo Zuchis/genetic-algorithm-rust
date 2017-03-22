e5c9f66ffcd8830bd25a9458d1bdf2802328eeab
Zuchis <liquimaster@gmail.com>
2017-03-21 22:31:38 -0300

    changed the fitness function signatures to fit the new population structure

--- src/fitness.rs
+++ src/fitness.rs
@@ -1,11 +1,8 @@
-use population::Population;
-
 #[allow(dead_code)]
-pub fn binary_alternate (pop: &Population<bool>, ind: u64) -> f64 {
-    let index = ind * pop.ind_size;
+pub fn binary_alternate (ind: &Vec<bool>) -> f64 {
     let mut fit: f64 = 0.0;
-    for j in 0 .. (pop.ind_size - 1) {
-        if pop.individuals[(index + j) as usize] != pop.individuals[(index + j + 1) as usize]{
+    for i in 0 .. (ind.len() - 1) {
+        if ind[i as usize] != ind[(i + 1) as usize]{
             fit = fit + 1.0;
         }
     }
@@ -13,13 +10,12 @@ pub fn binary_alternate (pop: &Population<bool>, ind: u64) -> f64 {
 }
 
 #[allow(dead_code)]
-pub fn int_parity_alternate (pop: &Population<i64>, ind: u64) -> f64 {
-    let index = ind * pop.ind_size;
+pub fn int_parity_alternate (ind: &Vec<i64>) -> f64 {
     let mut fit: f64 = 0.0;
-    for j in 0 .. (pop.ind_size - 1) {
-        let current: usize = (index + j) as usize;
-        let next: usize = (index + j + 1) as usize;
-        if ((pop.individuals[current] % 2 == 0) && (pop.individuals[next] % 2 != 0)) || ((pop.individuals[current] % 2 != 0) && (pop.individuals[next] % 2 == 0)){
+    for i in 0 .. (ind.len() - 1) {
+        let current: usize = i as usize;
+        let next: usize = i + 1 as usize;
+        if ((ind[current] % 2 == 0) && (ind[next] % 2 != 0)) || ((ind[current] % 2 != 0) && (ind[next] % 2 == 0)){
             fit = fit + 1.0;
         }
     }
@@ -27,11 +23,10 @@ pub fn int_parity_alternate (pop: &Population<i64>, ind: u64) -> f64 {
 }
 
 #[allow(dead_code)]
-pub fn float_quadratic_min (pop: &Population<f64>, ind: u64) -> f64 {
-    let index = ind * pop.ind_size;
+pub fn float_quadratic_min (ind: &Vec<f64>) -> f64 {
     let mut fit: f64 = 0.0;
-    for j in 0 .. (pop.ind_size - 1) {
-        let value = pop.individuals[(index + j) as usize];
+    for i in 0 .. ind.len() {
+        let value = ind[i as usize];
         fit = fit + (value * value);
     }
     1.0 / fit
