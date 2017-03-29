5db7b6e4ea523aecc4b82e4ab4c8d38fccb0b92b
Zuchis <liquimaster@gmail.com>
2017-03-29 16:55:59 +0100

    added a new function to calculate the max value between two floats

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,4 +1,4 @@
-pub fn bin_to_int(bstr: &Vec<bool>) -> i64 {
+pub fn bin_to_int (bstr: &Vec<bool>) -> i64 {
     let mut converted: i64 = 0;
     let mut power = 0;
     for i in (0..bstr.len()).rev() {
@@ -8,4 +8,9 @@ pub fn bin_to_int(bstr: &Vec<bool>) -> i64 {
         power += 1;
     }
     converted
+}
+
+pub fn maxf (x:f64, y:f64) -> f64 {
+    let max = if x >= y  {x} else {y};
+    max
 }
