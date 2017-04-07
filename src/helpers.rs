a0021bd7def2529368c59a805d873d0b47fa4117
Zuchis <liquimaster@gmail.com>
2017-04-07 22:31:20 +0100

    added a function to parse the configuration file

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,3 +1,7 @@
+use std::io::Read;
+use std::io::BufReader;
+use std::fs::File;
+
 pub fn bin_to_int (bstr: &Vec<bool>) -> i64 {
     let mut converted: i64 = 0;
     let mut power = 0;
@@ -13,4 +17,18 @@ pub fn bin_to_int (bstr: &Vec<bool>) -> i64 {
 pub fn maxf (x:f64, y:f64) -> f64 {
     let max = if x >= y  {x} else {y};
     max
+}
+
+pub fn parse_arguments() -> Vec<String> {
+    let file = File::open("ga.config").unwrap();
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents).unwrap();
+    let split = contents.split("\n");
+    let mut args: Vec<String> = Vec::new();
+    for s in split {
+        let arg = s.split("=");
+        args.push(arg.last().unwrap().to_string());
+    }
+    args
 }
