ed2457d13d392e16d1b09973f23de889df86a5d0
Zuchis <liquimaster@gmail.com>
2017-04-15 16:40:52 +0100

    added some pre-processor commands

--- src/crossover.rs
+++ src/crossover.rs
@@ -1,3 +1,7 @@
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+
 extern crate rand;
 
 use self::rand::Rng;
@@ -6,7 +10,7 @@ use self::rand::distributions::{IndependentSample, Range};
 
 pub fn one_point_crossover<T>(parent1: &mut Vec<T>, parent2: &mut Vec<T>)
     where T: Clone {
-    let cut_position: usize = Range::new(0usize, (parent1.len() - 1) as usize).ind_sample(&mut rand::thread_rng());
+    let cut_position: usize = Range::new(1usize, (parent1.len() - 1) as usize).ind_sample(&mut rand::thread_rng());
     let split_1 = parent1.split_off(cut_position);
     let split_2 = parent2.split_off(cut_position);
 
