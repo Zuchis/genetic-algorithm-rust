d8827e79c65b33fbbd06d96e0b150e9ab94a6ce4
Zuchis <liquimaster@gmail.com>
2017-04-15 16:42:24 +0100

    added two clamp functions, a print vector function, and a iterator with custom step

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,7 +1,8 @@
+use std::fmt;
 use std::io::Read;
 use std::io::BufReader;
 use std::fs::File;
-use std::process;
+use std::cmp;
 
 pub fn bin_to_int (bstr: &Vec<bool>) -> i64 {
     let mut converted: i64 = 0;
@@ -20,6 +21,15 @@ pub fn maxf (x:f64, y:f64) -> f64 {
     max
 }
 
+pub fn clamp<T> (value: T, lb: T, ub: T) -> T
+    where T: Ord {
+    cmp::min(ub,cmp::max(lb, value))
+}
+
+pub fn clampf (value: f64, lb: f64, ub: f64) -> f64 {
+    ub.min(lb.max(value))
+}
+
 pub fn parse_arguments() -> Vec<String> {
     let file = File::open("ga.config").unwrap();
     let mut buf_reader = BufReader::new(file);
@@ -32,4 +42,32 @@ pub fn parse_arguments() -> Vec<String> {
         args.push(arg.last().unwrap().to_string());
     }
     args
+}
+
+#[allow(dead_code)]
+pub fn print_vector<T>(vec: &Vec<T>)
+    where T: fmt::Display {
+    for i in 0 .. vec.len() {
+        print!("{} ", vec[i]);
+    }
+    println!("");
+}
+
+// http://stackoverflow.com/questions/27893223/how-do-i-iterate-over-a-range-with-a-custom-step
+#[allow(dead_code)]
+pub struct SimpleStepRange(pub usize, pub usize, pub usize);  // start, end, and step
+
+impl Iterator for SimpleStepRange {
+    type Item = usize;
+
+    #[inline]
+    fn next(&mut self) -> Option<usize> {
+    if self.0 < self.1 {
+        let v = self.0;
+        self.0 = v + self.2;
+        Some(v)
+    } else {
+        None
+    }
+    }
 }
