873d8a1859a975a48ff3bcb8286d13f430c6cd04
Zuchis <liquimaster@gmail.com>
2017-04-15 16:44:09 +0100

    fixed a problem where it had to be specified that the T had to have the clone method on the wheel function, and implemented the tournament

--- src/selection.rs
+++ src/selection.rs
@@ -1,11 +1,20 @@
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+
 extern crate rand;
 
+use std::process;
+
 use self::rand::Rng;
+use self::rand::thread_rng;
 use self::rand::distributions::{IndependentSample, Range};
 
 use population::Population;
 
-pub fn wheel<T>(pop: &mut Population<T>) -> usize {
+#[allow(dead_code)]
+pub fn wheel<T>(pop: &mut Population<T>) -> usize
+    where T: Clone {
     let sum: f64 = pop.fit_array.iter().fold(0.0, |a, &b| a + b);
     let roulete_position = Range::new(0.0, sum).ind_sample(&mut rand::thread_rng());
     let mut accumulator: f64 = 0.0;
@@ -18,4 +27,26 @@ pub fn wheel<T>(pop: &mut Population<T>) -> usize {
         }
     }
     chosen
+}
+
+#[allow(dead_code)]
+pub fn tournament<T>(pop: &mut Population<T>) -> usize
+    where T: Clone {
+    if pop.tournament_size > pop.pop_size {
+        println!("Error: The tournament size is bigger than the size of the population");
+        process::exit(1);
+    }
+    let mut indexes: Vec<usize> = (0usize .. pop.pop_size as usize).collect();
+    thread_rng().shuffle(&mut indexes);
+
+    let mut winner: usize = indexes.pop().unwrap();
+
+    for _ in 1..pop.tournament_size {
+        let next_challenger = indexes.pop().unwrap();
+        if pop.fit_array[next_challenger] > pop.fit_array[winner] {
+            winner = next_challenger;
+        }
+    }
+
+    winner
 }
