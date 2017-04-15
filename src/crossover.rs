ab99a0e5f5a50f0129acccda753ada140994c1f6
Zuchis <liquimaster@gmail.com>
2017-04-15 20:21:54 -0300

    started the implementation of the uniform crossover

--- src/crossover.rs
+++ src/crossover.rs
@@ -8,6 +8,7 @@ use self::rand::Rng;
 use self::rand::distributions::{IndependentSample, Range};
 
 
+#[allow(dead_code)]
 pub fn one_point_crossover<T>(parent1: &mut Vec<T>, parent2: &mut Vec<T>)
     where T: Clone {
     let cut_position: usize = Range::new(1usize, (parent1.len() - 1) as usize).ind_sample(&mut rand::thread_rng());
@@ -16,4 +17,19 @@ pub fn one_point_crossover<T>(parent1: &mut Vec<T>, parent2: &mut Vec<T>)
 
     parent1.extend_from_slice(&split_2);
     parent2.extend_from_slice(&split_1);
+}
+
+#[allow(dead_code)]
+pub fn uniform_crossover<T>(parent1: &mut Vec<T>, parent2: &mut Vec<T>)
+    where T: Clone {
+    let mut rng = rand::thread_rng();
+
+    for i in 0usize .. parent1.len() {
+        let fifty_fifty: bool = rng.gen::<bool>();
+        if fifty_fifty {
+            let value = &parent1[i];
+            parent1[i] = parent2[i];
+            parent2[i] = value;
+        }
+    }
 }
