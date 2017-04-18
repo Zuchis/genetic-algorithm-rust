cca2d7bc6bf79fb4e31edad0a0e1171dea42c37a
Zuchis <liquimaster@gmail.com>
2017-04-18 04:06:13 +0100

    implemented the gaussian mutation

--- src/mutation.rs
+++ src/mutation.rs
@@ -1,12 +1,15 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 #![allow(unused_mut)]
+#![allow(dead_code)]
 
 extern crate rand;
 
 use self::rand::Rng;
 use self::rand::distributions::{IndependentSample, Range};
 use self::rand::distributions::normal::StandardNormal;
+
+use std::f64;
 
 use ::helpers;
 
@@ -50,4 +53,23 @@ pub fn delta_mutation(pop: &mut Population<f64>, ind: usize, index: usize) {
     let sign = if fifty_fifty {1.0f64} else {-1.0f64};
     let ten_percent = interval.ind_sample(&mut rng) / 10.0f64;
     pop.individuals[ind][index] = helpers::clampf(pop.individuals[ind][index] + (ten_percent * sign),pop.lower_bound,pop.upper_bound);
+}
+
+pub fn gaussian_mutation(pop: &mut Population<f64>, ind: usize, index: usize) {
+    let delta = (pop.upper_bound - pop.lower_bound) / 10.0;
+
+    let mut x1 = rand::random::<f64>();
+    if x1 == 0.0 {
+        x1 = 1.0;
+    }
+
+    let mut x2 = rand::random::<f64>();
+    if x2 == 0.0 {
+        x2 = 1.0;
+    }
+
+    let y1 = (-2.0 * x1.ln()).sqrt() * (2.0 * f64::consts::PI * x2).cos();
+
+    pop.individuals[ind][index] = y1 * delta + pop.individuals[ind][index];
+
 }
