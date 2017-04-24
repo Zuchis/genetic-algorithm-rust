c7bd2991172cd34bb59dc2c02535aa2e01e36824
Zuchis <liquimaster@gmail.com>
2017-04-24 16:15:27 +0100

    testing some new functions

--- src/ga.rs
+++ src/ga.rs
@@ -15,9 +15,10 @@ pub fn int_loop(
     mut_function: fn(&mut Population<i64>, usize, usize),
     cross_function: fn(&mut Vec<i64>, &mut Vec<i64>),
     select_function: fn(&mut Population<i64>) -> usize,
+    perm: bool,
     ) {
     let mut pop = Population::<i64>::new(fit_function,mut_function,cross_function,select_function);
-    pop.initialize();
+    pop.initialize(perm);
     pop.evaluate_all();
     pop.print();
 
@@ -40,7 +41,7 @@ pub fn float_loop(
     mut_function: fn(&mut Population<f64>, usize, usize),
     cross_function: fn(&mut Vec<f64>, &mut Vec<f64>),
     select_function: fn(&mut Population<f64>) -> usize,
-) {
+    ) {
     let mut pop = Population::<f64>::new(fit_function,mut_function,cross_function,select_function);
     pop.initialize();
     pop.evaluate_all();
@@ -86,28 +87,15 @@ pub fn bin_loop(
 }
 
 pub fn testing () {
-    // let args: Vec<String> = helpers::parse_arguments();
-    // println!("Arguments given:");
-    // for i in args {
-    //     println!("{}",i);
-    // }
-
-    // println!("A display of the one-point crossover");
-
-    // let mut vec1: Vec<i64> = [1,2,3,4,5].to_vec();
-    // let mut vec2: Vec<i64> = [6,7,8,9,10].to_vec();
-    // crossover::one_point_crossover(&mut vec1, &mut vec2);
-
-    // helpers::print_vector(&vec1);
-    // helpers::print_vector(&vec2);
-
-    // println!("Testing the simple step range");
-    // for i in helpers::SimpleStepRange(0,10,2) {
-    //     print!("{} ", i);
-    // }
-    // println!("");
-
-    let ind: Vec<bool> = [true,false,true].to_vec();
-    let i: i64 = helpers::bin_to_int(&ind);
-    println!("{}",i);
+    let (is_in,pos) = helpers::is_in(&5,&[1,2,3,4,5].to_vec());
+    if is_in {
+        println!("the number 5 is in position {}",pos);
+    }
+    let mut vec1 = [1,2,3,4,5,6].to_vec();
+    let mut vec2 = [2,5,3,4,1,6].to_vec();
+    helpers::print_vector(&vec1);
+    helpers::print_vector(&vec2);
+    crossover::pmx_crossover(&mut vec1, &mut vec2);
+    helpers::print_vector(&vec1);
+    helpers::print_vector(&vec2);
 }
